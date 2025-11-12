afc54a6d2c43734e18427078c43622c899770b7e
Vadim Kislov <kislov.v@gmail.com>
2025-11-13 00:11:21 +0300

    Добавлены UI элементы и логика изменения счетчика

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,13 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var changeHistoryTextView: UITextView!
+    
+    private var count: Int = 0 {
+        willSet {
+            countLabel.text = String(newValue)
+        }
+    }
+    
+    private var currentDateTime: String {
+        Date.now.formatted(date: .numeric, time: .standard)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        changeHistoryTextView.text = "История изменений: \n"
     }
 
-
+    @IBAction func didTapIncrementButton() {
+        count += 1
+        
+        changeHistoryTextView.text += "[\(currentDateTime)]: значение изменено на +1 \n"
+    }
+    
+    @IBAction func didTapDecrementButton() {
+        if (count == 0) {
+            changeHistoryTextView.text += "[\(currentDateTime)]: попытка уменьшить значение счётчика ниже 0 \n"
+            
+            return
+        }
+        
+        count -= 1
+        
+        changeHistoryTextView.text += "[\(currentDateTime)]: значение изменено на -1 \n"
+    }
+    
+    @IBAction func didTapResetButton() {
+        count = 0
+        
+        changeHistoryTextView.text += "[\(currentDateTime)]: значение сброшено \n"
+    }
 }
-
